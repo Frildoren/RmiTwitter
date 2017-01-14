1237147f693d4e7136a86e2e8c7590366f722e14
Roberto Garrido <garrihendrix@gmail.com>
2017-01-14 13:10:58 +0100

    User model added

---
+++ src/common/models/User.java
@@ -0,0 +1,53 @@
+package common.models;
+
+import java.util.List;
+
+public class User {
+
+    private String name;
+    private String nick;
+    private String password;
+
+    private List<Tweet> tweets;
+    private List<User> following;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Tweet> getTweets() {
+        return tweets;
+    }
+
+    public void setTweets(List<Tweet> tweets) {
+        this.tweets = tweets;
+    }
+
+    public List<User> getFollowing() {
+        return following;
+    }
+
+    public void setFollowing(List<User> following) {
+        this.following = following;
+    }
+}
