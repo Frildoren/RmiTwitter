78fbce941f3396cb9097cbd503b3217844c75757
Roberto Garrido <garrihendrix@gmail.com>
2017-01-14 16:50:55 +0100

    mainView updated

--- src/client/main/MainView.java
+++ src/client/main/MainView.java
@@ -1,10 +1,12 @@
 package client.main;
 
 import client.base.FrameView;
+import client.base.View;
 import common.models.User;
 
 public interface MainView extends FrameView<MainPresenter> {
 
     void setUser(User user);
+    void setNestedView(View view);
 
 }
