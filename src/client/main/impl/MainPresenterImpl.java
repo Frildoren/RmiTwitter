fd1e271964f732073cb6ba130d459aecf106a9f7
Pablo Garcia <yinxro@gmail.com>
2017-01-14 19:01:46 +0100

    Creation of MainView

--- src/client/main/impl/MainPresenterImpl.java
+++ src/client/main/impl/MainPresenterImpl.java
@@ -1,13 +1,19 @@
 package client.main.impl;
 
 import client.base.impl.BasePresenter;
+import client.login.impl.LoginPresenterImpl;
 import client.main.MainPresenter;
 import client.main.MainView;
 
+import java.rmi.RemoteException;
+
 public class MainPresenterImpl extends BasePresenter<MainView> implements MainPresenter {
+
     @Override
     protected MainView createView() {
-        return null;
+        MainViewImpl mainView = new MainViewImpl();
+        mainView.create(this);
+        return mainView;
     }
 
     @Override
@@ -22,6 +28,18 @@ public class MainPresenterImpl extends BasePresenter<MainView> implements MainPr
 
     @Override
     public void onDisconnect() {
+
+        try {
+            if(getClient().getServer().getUserManager().disconnect( getClient().getUser().getNick() )){
+                getClient().setUser(null);
+                createPresenter(LoginPresenterImpl.class);
+                finish();
+            } else {
+                getView().showError("Unexpected error disconnecting");
+            }
+        } catch (RemoteException e) {
+            getView().showError("Connection error");
+        }
 
     }
 }
