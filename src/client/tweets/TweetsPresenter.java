dac56793a8a223a266e5d470997e0bb80fbc25f4
Pablo Garcia <yinxro@gmail.com>
2017-01-15 11:44:05 +0100

    Added method to set tweets in tweets presenter

--- src/client/tweets/TweetsPresenter.java
+++ src/client/tweets/TweetsPresenter.java
@@ -1,9 +1,12 @@
 package client.tweets;
 
 import client.base.Presenter;
+import common.models.Tweet;
+
+import java.util.List;
 
 public interface TweetsPresenter extends Presenter<TweetsView> {
 
 
-
+    void setTweets(List<Tweet> tweets);
 }
