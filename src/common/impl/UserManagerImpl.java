327f0e54141e3e9a85708e99238d34f127feb7b7
Roberto Garrido <garrihendrix@gmail.com>
2017-01-15 21:28:03 +0100

    userManager error in connect fixed

--- src/common/impl/UserManagerImpl.java
+++ src/common/impl/UserManagerImpl.java
@@ -20,14 +20,15 @@ public class UserManagerImpl extends UnicastRemoteObject implements UserManager 
     @Override
     public User connect(String nick, String password) throws RemoteException {
 
-        User user = null;
+        User user = usersRegistered.get(nick);
 
-        if((user=usersRegistered.get(nick)) != null) {
-            if(user.getPassword().equals(password))
-                usersConnected.put(nick, user);
+        if(user != null && user.getPassword().equals(password)) {
+            usersConnected.put(nick, user);
+            return user;
+        } else {
+            return null;
         }
 
-        return user;
     }
 
     @Override
