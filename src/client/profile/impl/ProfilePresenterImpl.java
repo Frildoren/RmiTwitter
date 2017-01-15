caecd6913ce22b6822b050650479c3c6e0a2c97e
Pablo Garcia <yinxro@gmail.com>
2017-01-15 23:53:38 +0100

    Implemented new methods for profile following/unfollowing

--- src/client/profile/impl/ProfilePresenterImpl.java
+++ src/client/profile/impl/ProfilePresenterImpl.java
@@ -6,6 +6,8 @@ import client.profile.ProfilePresenter;
 import client.profile.ProfileView;
 import common.models.User;
 
+import java.rmi.RemoteException;
+
 public class ProfilePresenterImpl extends BasePresenter<ProfileView> implements ProfilePresenter {
 
     @Override
@@ -18,11 +20,50 @@ public class ProfilePresenterImpl extends BasePresenter<ProfileView> implements 
     @Override
     public void setUser(User user) {
         getView().setUser(user);
+        updateFollowing(user);
+    }
+
+    private void updateFollowing(User user){
+        try {
+            if(user.getNick().equals(getClient().getUser().getNick())){
+                getView().setFollowVisible(false);
+                getView().setUnfollowVisible(false);
+                getView().setMessageVisible(false);
+            } else {
+                boolean following = getClient().getUser().isFollowing(user);
+                getView().setFollowVisible(!following);
+                getView().setUnfollowVisible(following);
+            }
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
-    public void onUserFollowing(User user) {
+    public void onUserFollow(User user) {
+        try {
+            getClient().getUser().follow(user);
+            updateFollowing(user);
+        } catch (RemoteException e) {
+            getView().showError("Connection error");
+            e.printStackTrace();
+        }
+    }
 
+    @Override
+    public void onUserUnfollow(User user) {
+        try {
+            getClient().getUser().unfollow(user);
+            updateFollowing(user);
+        } catch (RemoteException e) {
+            getView().showError("Connection error");
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void sendMessage(User user) {
+        getView().showError("Not implemented yet");
     }
 
 
